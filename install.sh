eb3320a796abc8f6819f01a33439f2c5df9f3d25
matsushimen <ukasustam@gmail.com>
2020-03-12 01:09:20 +0900

    add install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+sudo apt-get install libbz2-dev libreadline-dev libsqlite3-dev libffi-dev
+git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
+source ~/.bash_profile
+pyenv install 3.8.2
+pip install --upgrade pip
+pip install prompt-toolkit xonsh
+echo 'xonsh' >> ~/.bash_profile
+sudo apt install tmux
+cp .tmux.conf ~
+cp .xonshrc ~
+cp .vimrc ~
+cp .gvimrc ~
+echo 'tmux' >> ~/.bashrc
+source ~/.bashrc
